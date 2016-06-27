4801a407f1daa84ea38f4a22b7b3f564ce9defc2
Patrick Hempel <hempel@flinsta.com>
2016-06-27 11:00:46 +0200

    starting to build a small marionette app for the frontend

---
+++ public/javascripts/main.js
@@ -0,0 +1,149 @@
+$(document).ready( function($) {
+
+    var app = new Marionette.Application();
+
+    var vent = new Backbone.Wreqr.EventAggregator();
+
+    //Models
+    var ExchangeUpdate = Backbone.Model.extend({});
+    var WatchExchange = Backbone.Model.extend({
+        defaults: {
+            type: 'WatchExchange'
+        }
+    });
+    var UnwatchExchange = Backbone.Model.extend({
+        defaults: {
+            type: 'UnwatchExchange'
+        }
+    });
+
+
+    //Views
+    var RootView = Marionette.LayoutView.extend({
+                el: '#root',
+                template: '#rootTemplate',
+                regions: {
+                    chart: '#chart'
+                }
+            });
+
+    app.rootView = new RootView();
+
+
+    var ChartView = Marionette.ItemView.extend({
+        initialize: function() {
+            //start listening on the vent
+            vent.on('ExchangeUpdate', this.onExchangeUpdate, this);
+            vent.on('UnwatchExchange', this.onUnwatchExchange, this);
+
+        },
+        template: "#chart",
+        onExchangeUpdate: function( exchangeUpdate) {
+            //search for the series of exchange
+            var series = this.chart.get( exchangeUpdate.get('exchange'));
+
+            if( series === null) {
+                //create new series
+                this.chart.addSeries({
+                    id: exchangeUpdate.get('exchange'),
+                    name: exchangeUpdate.get('exchange'),
+                    data: [
+                        [exchangeUpdate.get('timestamp'),
+                        exchangeUpdate.get('bid')]
+                    ]
+                });
+            } else {
+                //adding new point to series
+                var point = [
+                    exchangeUpdate.get('timestamp'),
+                    exchangeUpdate.get('bid')
+                ];
+                series.addPoint(point, true, false, false);
+            }
+
+        },
+        onUnwatchExchange: function( unwatchExchange) {
+            var series = this.chart.get( unwatchExchange.get('exchange'));
+
+            if( series !== null) {
+                series.remove(true);
+            }
+        },
+        onRender: function() {
+            //setup chart
+            this.chart = new Highcharts.Chart({
+                chart: {
+                    renderTo: 'chart'
+                },
+                title: {
+                    text: 'Bitcoin'
+                },
+                xAxis: {
+                   type: 'datetime'
+                },
+                yAxis: {
+                   title: {
+                       text: 'EUR'
+                   }
+                }
+             });
+        }
+    });
+
+
+    /*
+     Simple wrapper to hold a WebSocket
+     */
+    var BackendSocket = function() {
+        var url = "ws://" + window.location.host + "/ticker";
+
+        this.connect = function() {
+
+            console.log("Trying to connect to Backend via Websocket");
+
+            var socket = new WebSocket( url);
+
+            socket.onopen = function( event) {
+                vent.on("WatchExchange UnwatchExchange", function( event) {
+                    socket.send( JSON.stringify(event.toJSON()));
+                });
+
+                vent.trigger("SocketOpen");
+            };
+
+            socket.onclose = function( event) {
+            };
+
+            socket.onmessage = function( event) {
+                var response = JSON.parse( event.data);
+
+                switch(response.type) {
+                    case "ExchangeUpdate":
+                        vent.trigger("ExchangeUpdate", new ExchangeUpdate( response));
+                    break;
+                }
+            };
+
+            socket.onerror = function( event) {
+
+            };
+
+            return socket;
+        }
+    };
+
+    app.on('start', function() {
+        app.rootView.render();
+        Backbone.history.start();
+
+        var backendSocket = new BackendSocket();
+        backendSocket.connect();
+    });
+
+    vent.on("SocketOpen", function() {
+        app.rootView.getRegion("chart").show( new ChartView());
+    });
+
+    app.start();
+});
+
